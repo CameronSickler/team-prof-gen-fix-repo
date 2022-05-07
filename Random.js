cbc51d986da3ecffd0d1ddd819d9177a9e6f315e
Cameron <sicklecj@gmail.com>
2022-05-07 16:13:31 -0500

    very broken, unable to fix in time of deadline likely

--- Random.js
+++ Random.js
@@ -7,17 +7,15 @@ const initializeEmployee = require('./lib/Employee');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const askManagerQuestions = require('./Manager');
-const askEngineerQuestions = require('./Engineer');
-const askInternQuestions = require('./Intern');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 const generateHTML = require('./generateHTML');
 const generateCSS = require('./generateCSS');
+const Employee = require('./lib/Employee');
 
 //to hold created employee objects in array but not working atm
-const engineersArray = [];
-const managersArray = [];
-const internsArray = [];
 
 var currentUserSelection
 
@@ -104,53 +102,54 @@ const managerQuestions = [{
 
 //function to ask user what employee to add or if done adding employees
 
-
-
-async function initializeEmployee() {
-    //inquirer prompt
-    try {
-        const answers = await inquirer.prompt(menuQuestion);
-        currentUserSelection = answers.menuSelection;
-        employeeInformationGenerator()
-
-
-    } catch (error) {
-        if (error.isTtyError) {
-
-            console.log('error');
-
-        } else {
-
-            console.log('something else error');
-
-        }
-    }
+function Generator() {
+    this.engineersArray = [];
+    this.managersArray = [];
+    this.internsArray = [];
 }
 
+Generator.prototype.initializeEmployee = async function () {
 
-//function that uses if/else to call a function depending on user choice
-// Manager, Engineer, Intern, or Done
+    inquirer.prompt({
+        type: 'list',
+        name: 'menuSelection',
+        message: 'Add one of the following or select done if finished.',
+        choices: [
+            'Manager',
+            'Engineer',
+            'Intern',
+            'Done'
+        ]
+    })
 
-async function employeeInformationGenerator() {
+        .then(({ menuSelection }) => {
+            this.employee = new Employee();
+            currentUserSelection = menuSelection;
+            this.employeeInformationGenerator()
+        });
 
+
+}
+
+Generator.prototype.employeeInformationGenerator = async function () {
 
     if (currentUserSelection == 'Manager') {
 
-        const managerAdd = await askManagerQuestions();
+        const managerAdd = await new Manager();
         console.log('passed through askManagerQuestion')
         managersArray.push(managerAdd);
         reinitialize();
 
     } else if (currentUserSelection == 'Engineer') {
 
-        const engineerAdd = await askEngineerQuestions();
+        const engineerAdd = await new Engineer();
         console.log('passed through askEngineerQuestion')
         engineersArray.push(engineerAdd);
         reinitialize();
 
     } else if (currentUserSelection == 'Intern') {
 
-        const internAdd = await askInternQuestions();
+        const internAdd = await new Intern();
         console.log('passed through askInternQuestion')
         internsArray.push(internAdd);
         reinitialize();
@@ -178,12 +177,7 @@ async function employeeInformationGenerator() {
 
 }
 
-
-//function to determine if user requested to add another employee and
-//if so, calls the initialEmployee function again to loop user through
-
-function reinitialize() {
-
+Generator.prototype.reinitialize = function () {
     if (currentUserSelection !== 'Done') {
 
         console.log('keeping it going from reinitialize function')
@@ -194,13 +188,40 @@ function reinitialize() {
         console.log('reinitialize else block executed')
 
     }
+}
+
+Generator.prototype.askManagerQuestions = async function () {
+
+    const answers = await inquirer.prompt(managerQuestions);
+    const manager = new Manager(answers.name, answers.ID, answers.email, answers.officeNumber, role);
+    console.log(manager);
+    return manager;
+}
+
+Generator.prototype.askInternQuestions = async function () {
+
+    const answers = await inquirer.prompt(internQuestions);
+    const intern = new Intern(answers.name, answers.ID, answers.email, answers.school);
+    console.log(intern);
+    return intern;
+
+}
+
+Generator.prototype.askEngineerQuestions = async function () {
+
+    const answers = await inquirer.prompt(engineerQuestions);
+    const engineer = new Engineer(answers.name, answers.ID, answers.email, answers.username);
+    console.log(engineer);
+    return engineer;
 
 }
 
 
-module.exports = initializeEmployee
 
 
 
 
-initializeEmployee();
+// module.exports = initializeEmployee
+
+
+new Generator().initializeEmployee();
